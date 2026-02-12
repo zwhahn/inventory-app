f5daa8658940f1da6a2db4fed0f695cb2bf07e6c
zwhahn <117476820+zwhahn@users.noreply.github.com>
2026-02-12 13:19:43 -0500

    feat: create boilerplate express server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello, world"));
+
+const PORT = 3000;
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`App listening on port ${PORT}`);
+});
