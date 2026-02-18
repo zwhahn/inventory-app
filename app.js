968a6e23ad6efb029ad36253d130534eb9bd5c50
zwhahn <117476820+zwhahn@users.noreply.github.com>
2026-02-18 14:07:16 -0500

    style: connect style sheet and add table borders

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 app.use("/", indexRouter);
 
 const PORT = 3000;
