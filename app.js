402990873fab2ba901e83ec9ae4c8a54598ffa1f
zwhahn <117476820+zwhahn@users.noreply.github.com>
2026-02-18 11:20:00 -0500

    build: set view engine to ejs

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const indexRouter = require("./routes/indexRouter");
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
 
