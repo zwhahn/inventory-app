ed2cb816feeb1bb61912cc899cfb7265fd441bd3
zwhahn <117476820+zwhahn@users.noreply.github.com>
2026-02-18 11:20:18 -0500

    feat: render view of all items

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,7 +3,9 @@ const db = require("../db/queries");
 async function getAllProducts(req, res) {
   try {
     const products = await db.getAllProducts();
-    res.send(products);
+    res.render("index", {
+      items: products,
+    });
   } catch (err) {
     console.error("Error fetching messages: ", err);
     res.status(500).send("Error loading messages");
--- db/queries.js
+++ db/queries.js
@@ -3,6 +3,7 @@ const pool = require("./pool");
 
 async function getAllProducts() {
   const { rows } = await pool.query("SELECT * FROM items");
+  console.log("rows: ", rows);
   return rows;
 }
 
